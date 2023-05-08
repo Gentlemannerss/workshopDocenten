fa6c8ee1eede4053adcfd6b9ddd1083865994c27
Gentlemannerss <damonvanbergen@gmail.com>
2023-05-08 16:26:27 +0200

    first commit

---
+++ src/main/java/com/example/workshopdocenten/WorkshopDocentenApplication.java
@@ -0,0 +1,13 @@
+package com.example.workshopdocenten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopDocentenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopDocentenApplication.class, args);
+    }
+
+}
